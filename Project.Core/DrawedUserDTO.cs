6ab6aea56586303908767138426f40587174d903
filizsalnur <92436947+filizsalnur@users.noreply.github.com>
2022-08-31 14:37:21 +0300

    Add project files.

---
+++ Project.Core/DrawedUserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Core
+{
+    public class DrawedUserDTO
+    {
+        public Person ChoserUser { get; set; }
+
+        public Person ChosenUser { get; set; }
+
+
+    }
+}
---
+++ Project.Core/MatchedUserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Core
+{
+    public class MatchedUserDTO
+    {
+        public Person FirstUser { get; set; }
+
+        public Person SecondUser { get; set; }
+    }
+}
